0c8535d6a9eb1ac3a9d510809f0020706e42b334
Howard Kim <howard.mkf@gmail.com>
2021-06-02 10:34:28 -0700

    remove fb login

--- public/js/index.js
+++ public/js/index.js
@@ -7,36 +7,37 @@ function initializePage() {
 
     if (localStorage.loginState != null) {
         changeUser();
-    } else {
-        $("hr").hide();
-        $("nav").hide();
-    }
+    } 
+    // else {
+    //     $("hr").hide();
+    //     $("nav").hide();
+    // }
 }
 
-function saveUser(response) {
-    // $("#profile-img").attr("src", response.picture.data.url)
-    $("#greeting").text("Hello " + response.first_name + "!");
-    localStorage.setItem("loginState", true);
-    // localStorage.setItem("userPic", response.picture.data.url);
-    localStorage.setItem("userName", response.first_name);
-}
+// function saveUser(response) {
+//     // $("#profile-img").attr("src", response.picture.data.url)
+//     $("#greeting").text("Hello " + response.first_name + "!");
+//     localStorage.setItem("loginState", true);
+//     // localStorage.setItem("userPic", response.picture.data.url);
+//     localStorage.setItem("userName", response.first_name);
+// }
 
-function authUser() {
-    FB.login(checkLoginStatus);
-}
+// function authUser() {
+//     FB.login(checkLoginStatus);
+// }
 
-function checkLoginStatus(response) {
-    // log fb login change
-    console.log('Checking Facebook Login Status');
-    console.log(response);
+// function checkLoginStatus(response) {
+//     // log fb login change
+//     console.log('Checking Facebook Login Status');
+//     console.log(response);
 
-    if (response && response.status == 'connected') {
-        // Logged into your app and Facebook.
-        console.log('Successfully logged in with Facebook');
-        FB.api('/me?fields=name,first_name,picture.width(480)', saveUser);
-        changeUser();
-    }
-}
+//     if (response && response.status == 'connected') {
+//         // Logged into your app and Facebook.
+//         console.log('Successfully logged in with Facebook');
+//         FB.api('/me?fields=name,first_name,picture.width(480)', saveUser);
+//         changeUser();
+//     }
+// }
 
 function changeUser() {
     // get user packages from user's local storage
@@ -56,33 +57,33 @@ function changeUser() {
         console.log(res);
     }
 
-    $(".Login").hide();
+    // $(".Login").hide();
     $("hr").show();
     getNav();
     $("nav").show();
     // $("#profile-img").attr("src", localStorage.userPic)
-    $("#greeting").text("Hello " + localStorage.userName + "!");
+    // $("#greeting").text("Hello " + localStorage.userName + "!");
 }
 
-function logout() {
-    localStorage.removeItem("loginState");
-    // localStorage.removeItem("userPic");
-    localStorage.removeItem("userName");
-    FB.logout(function (response) {
-        console.log('Facebook login status changed.');
-    });
-    $(".Login").show();
-    $("hr").hide();
-    $("nav").hide();
-    // $("#profile-img").attr("src", "//ssl.gstatic.com/accounts/ui/avatar_2x.png")
-    $("#greeting").text("");
-}
+// function logout() {
+//     localStorage.removeItem("loginState");
+//     // localStorage.removeItem("userPic");
+//     localStorage.removeItem("userName");
+//     FB.logout(function (response) {
+//         console.log('Facebook login status changed.');
+//     });
+//     $(".Login").show();
+//     $("hr").hide();
+//     $("nav").hide();
+//     // $("#profile-img").attr("src", "//ssl.gstatic.com/accounts/ui/avatar_2x.png")
+//     $("#greeting").text("");
+// }
 
 function getNav() {
 
     var navHTML = '<a href="/calculator">Shipping Calculator</a>' +
-        '<a href="/mypackages">My Packages</a>' +
-        '<a onclick="logout();">Logout</a>';
+        '<a href="/mypackages">My Packages</a>' ;
+        // + '<a onclick="logout();">Logout</a>';
 
     $(".navCol").html(navHTML);
 }
